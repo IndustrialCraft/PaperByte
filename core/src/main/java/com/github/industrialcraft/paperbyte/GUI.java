a7adc1b2fe2864dbecbf38c2f99a6c738a9615d8
mmm <aaa123@pobox.sk>
2023-01-23 18:59:28 +0100

    changed ui a bit

--- core/src/main/java/com/github/industrialcraft/paperbyte/GUI.java
+++ core/src/main/java/com/github/industrialcraft/paperbyte/GUI.java
@@ -22,7 +22,7 @@ public class GUI {
     public GUI() {
         this.shapeRenderer = new ShapeRenderer();
         this.spriteBatch = new SpriteBatch();
-        this.camera = new OrthographicCamera(200, 200);
+        this.camera = new OrthographicCamera(2000, 2000);
         this.shapeRenderer.setProjectionMatrix(camera.combined);
         this.spriteBatch.setProjectionMatrix(camera.combined);
         this.shapeRenderer.setAutoShapeType(true);
@@ -30,20 +30,22 @@ public class GUI {
         this.uiComponents = Collections.EMPTY_LIST;
     }
     public void draw(){
-        spriteBatch.begin();
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         for(BasicUIComponent component : uiComponents){
             if(component instanceof RectUIComponent rectUIComponent){
+                shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
                 shapeRenderer.setColor(rectUIComponent.color);
                 shapeRenderer.rect(rectUIComponent.x, rectUIComponent.y, rectUIComponent.width, rectUIComponent.height);
+                shapeRenderer.end();
             }
             if(component instanceof TextUIComponent textUIComponent){
+                spriteBatch.begin();
                 spriteBatch.setColor(textUIComponent.color);
+                font.getData().setScale(textUIComponent.scale);
                 font.draw(spriteBatch, textUIComponent.text, textUIComponent.x, textUIComponent.y);
+                font.getData().setScale(1/textUIComponent.scale);
+                spriteBatch.end();
             }
         }
-        spriteBatch.end();
-        shapeRenderer.end();
     }
     public void fromPacket(SetGUIPacket packet){
         this.uiComponents = packet.uiComponents.stream().toList();
--- shared/src/main/java/com/github/industrialcraft/paperbyte/common/gui/TextUIComponent.java
+++ shared/src/main/java/com/github/industrialcraft/paperbyte/common/gui/TextUIComponent.java
@@ -10,18 +10,22 @@ public class TextUIComponent extends BasicUIComponent{
     public static final byte SER_ID = 2;
 
     public final String text;
-    public TextUIComponent(Color color, float x, float y, String text) {
+    public final float scale;
+    public TextUIComponent(Color color, float x, float y, String text, float scale) {
         super(color, x, y);
         this.text = text;
+        this.scale = scale;
     }
     public TextUIComponent(DataInputStream stream) throws IOException {
         super(stream);
         this.text = stream.readUTF();
+        this.scale = stream.readFloat();
     }
     @Override
     public void toStream(DataOutputStream stream) throws IOException {
         super.toStream(stream);
         stream.writeUTF(text);
+        stream.writeFloat(scale);
     }
 
     @Override
